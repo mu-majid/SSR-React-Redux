43df9863b0a4a63f6d90899b9a48a56f38365899
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-10 15:40:26 +0200

    separate ssr logic from express logic

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,14 @@
 // Root file for server bundle
 
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import Renderer from './helpers/renderer';
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
 
-  const content = renderToString(<Home />);
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-  `;
-
-  return res.send(html);
+  return res.send(Renderer());
 });
 
 app.listen(3000, () => {
