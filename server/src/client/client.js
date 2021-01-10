373d3ba39ef702189448b1842ce96f1fa3793884
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-10 20:50:33 +0200

    client side implementation of redux store

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -5,12 +5,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'; // React component used to communicate data between store and other `connected` components
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
+,
   // this will take over the div rendered and sent by the server initially
   // We do this to mount/register our event handlers, ...
   document.querySelector('#root') 
