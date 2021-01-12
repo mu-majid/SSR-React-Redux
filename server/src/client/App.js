2982492cbb2edc47f00f297494202656c032d9e1
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-12 19:27:02 +0200

    add header component

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -1,12 +1,13 @@
 import react from 'react';
 import { renderRoutes } from 'react-router-config';
+import Header from './components/Header';
 
 // route here is the nested/child components to show inside the App (check Routes.js)
 const App = ({ route }) => {
   return (
     <div>
-      <h1>Header Lives Here</h1>
-      <div>{renderRoutes(route.routes)}</div>
+      <Header />
+      {renderRoutes(route.routes)}
     </div>
   );
 };
