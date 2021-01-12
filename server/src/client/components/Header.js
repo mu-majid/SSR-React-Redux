2c5e7ce7787c5918b61a4f3ad7f59017cf4416d3
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-12 19:42:16 +0200

    connecting header component to redux store

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default () => {
+const Header = ({ authenticated }) => {
+  console.log('authenticated ', authenticated)
+
   return (
     <div>
       <Link to="/">SSR-React</Link>
     </div>
   )
 };
+
+function mapStateToProps ({ authenticated }) {
+  return { authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
